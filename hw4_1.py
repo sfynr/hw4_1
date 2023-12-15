572cd844c21d9cdedb34ed878633a290698f32d7
sfynr <151222282+sfynr@users.noreply.github.com>
2023-12-15 11:26:01 +0300

    Add files via upload

---
+++ hw4_1.py
@@ -0,0 +1,32 @@
+import math
+
+class Shape:
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius**2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def sum_of_internal_angels(self):
+        return (self.width + self.height - 2) * 180
